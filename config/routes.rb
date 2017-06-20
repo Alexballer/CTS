0bf01dd8d2e6f431cba3af8dbd9a16cc059d3660
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 17:40:23 +0000

    Integrated index action for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
-  resources :featurenames
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
