399a94474fb29ec4b712d52c70a604c83d8ca630
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 14:57:18 +0000

    app folder

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
-class Blog < ApplicationRecord
-  enum status: { draft: 0, published: 1 }
-  extend friendlyId
-  friendly_Id :title, use: :slugged
+ class Blog < ApplicationRecord
+   enum status: { draft: 0, published: 1 }
+   extend FriendlyId
+   friendly_id :title, use: :slugged
+  
+  validates_presence_of :title, :body
 end
