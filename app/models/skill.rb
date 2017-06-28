db54fbc09a26067fc319a205848032fc3d25a634
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 14:38:03 +0000

    Implemented concern for managing image data

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,10 @@
 class Skill < ApplicationRecord
-validates_presence_of :title, :percent_utilized
+    include Placeholder
+    validates_presence_of :title, :percent_utilized
+    
+    after_initalize :set_defaults
+    
+    def set_defaults
+        self.badge ||= Placeholder.image_generator(height: '350', width:'250')
+    end
 end
