0b4fe5c5476e70fd7ccf49246f3588581f28a9e8
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 16:49:26 +0000

    Integrated Nested attributes for portfolio model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,8 +1,5 @@
 class Skill < ApplicationRecord
-    include Placeholder
     validates_presence_of :title, :percent_utilized
-    
-    after_initalize :set_defaults
     
     def set_defaults
         self.badge ||= Placeholder.image_generator(height: '350', width:'250')
