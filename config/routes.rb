ae1702bb8ad1da607161fc7a3e260d01fe850485
Jordan Hudgens <jordan@devcamp.com>
2017-07-05 17:12:58 +0000

    Added session tracker for dynamic source links

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   resources :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :portfolios, except: [:show]
+  get'angular-items', to: 'portfolios#angular'
   get 'portfiolo/:id', to: 'portfolios#show', as: 'portfolio_show'
   
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
 
-  resources :blogs
+  resources :blogs do 
+    member do
+      get :toggle_status
+    end
+  end
   
   root to: 'pages#home'
 end
